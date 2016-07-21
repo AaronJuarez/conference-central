2a5d22e75bde2167ff9d2c2bf6330ea3e133e9f3
Aaron Juarez <aarjuamtz@gmail.com>
2016-07-21 13:15:06 -0500

    Feat: CLIENT_ID added

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'replace with your client id',
+        CLIENT_ID: '124917387198-v2dmtq66l3smbdro2bqf5jj2r5erchr9.apps.googleusercontent.com',
         SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
         signedIn: false
     };
